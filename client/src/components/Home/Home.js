d752ef2ad87725d72e686827bdcc8a6f5d926f61
KosmosKey <khamidovmaga2002@gmail.com>
2020-07-30 19:53:27 +0200

    I am on half way of finishing web

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,14 +1,18 @@
-import React, { Fragment } from "react";
+import React from "react";
 import Header from "../../Logo/header-bg.png";
 import iPhone from "../../Logo/iPhone.png";
+import firstSvg from "../../Logo/svg1.png";
+import secondSvg from "../../Logo/svg2.png";
+import Event from "../../Logo/Event1.png";
+import thirdSvg from "../../Logo/svg3.png";
 import { Grid, Container, Button } from "@material-ui/core";
 import "./Home.scss";
 
 function Home() {
   return (
-    <div className="Home__Container">
+    <div>
       <img src={Header} className="Home__ImageWave" alt="wave" />
-      <Container>
+      <Container className="Home__Container">
         <Grid container direction="row" justify="center" alignItems="center">
           <Grid
             item
@@ -18,27 +22,31 @@ function Home() {
             alignItems="center"
             xs={12}
             sm={12}
-            lg={6}
+            lg={8}
           >
             <div className="Home__Text_Presentation">
-              <h1>
-                START MAKING NEW,
-                <br />
-                CHANGES TO YOUR LIFE
-              </h1>
+              <h1>START MAKING NEW, CHANGES TO YOUR LIFE</h1>
               <p>
                 This Hobby maker web application, You can see how many hobbies
-                you have, have a plan when to complete your hobbies, What
-                hobbies made you succeed and what hobbies made you not succeed.
-                There is so much more than that. Just sign up and give a try to
-                this web app!
+                you have, have a plan when
+                <br /> to complete your hobbies, What hobbies made you succeed
+                and what hobbies made you not succeed. There is so much more
+                than that. Just sign up and give a try to this web app!
               </p>
               <div className="Home__Buttons">
-                <Button variant="contained" color="primary">
-                  Primary
+                <Button
+                  variant="contained"
+                  className="Home__GetStarted"
+                  color="primary"
+                >
+                  GET STARTED
                 </Button>
-                <Button vairant="contained" color="primary">
-                  Primary
+                <Button
+                  vairant="contained"
+                  className="Home__LogIn"
+                  color="primary"
+                >
+                  LOG IN
                 </Button>
               </div>
             </div>
@@ -47,7 +55,7 @@ function Home() {
             item
             xs={12}
             sm={12}
-            lg={6}
+            lg={4}
             container
             direction="row"
             justify="center"
@@ -58,6 +66,112 @@ function Home() {
           </Grid>
         </Grid>
       </Container>
+      <div className="Home__WhySection">
+        <section>
+          <h1 className="Home__WhyText">WHY HOBBYMAKER?</h1>
+          <Container>
+            <Grid container justify="center" alignItems="center">
+              <Grid
+                item
+                container
+                direction="row"
+                justify="center"
+                alignItems="center"
+                lg={4}
+              >
+                <div className="Home__first">
+                  <img
+                    src={firstSvg}
+                    className="Home__firstImage"
+                    alt="third"
+                  />
+                  <h1>Share your hobbies and ideas with friends & family</h1>
+                </div>
+              </Grid>
+              <Grid
+                item
+                container
+                direction="row"
+                justify="center"
+                alignItems="center"
+                lg={4}
+              >
+                <div className="Home__first">
+                  <img
+                    src={secondSvg}
+                    className="Home__secondImage"
+                    alt="third"
+                  />
+                  <h1>Structure your ideas very easily throughout the days</h1>
+                </div>
+              </Grid>
+              <Grid
+                item
+                container
+                direction="row"
+                justify="center"
+                alignItems="center"
+                lg={4}
+              >
+                <div className="Home__first">
+                  <img
+                    src={thirdSvg}
+                    className="Home__thirdImage"
+                    alt="third"
+                  />
+                  <h1>Explore new things within the app and the world!</h1>
+                </div>
+              </Grid>
+            </Grid>
+          </Container>
+        </section>
+      </div>
+      <div className="Home__SetContactGoal">
+        <Container>
+          <Grid container justify="center">
+            <Grid
+              item
+              direction="row"
+              justify="center"
+              alignItems="center"
+              lg={6}
+              sm={6}
+            >
+              <div className="Home__SetGoalsForHobi">
+                <h1>
+                  Set your self future goals with
+                  <br /> your passion and hobbies
+                </h1>
+                <p>
+                  You've heard people telling you go and do what you love doing.
+                  That's right! This app allows you to have so many
+                  oppurtunitues.
+                </p>
+                <p className="Home__paragraph">
+                  Such as How many hobbies you have, your plans for the hobbies,
+                  hobbies that did go so well and hobbies that went well and
+                  etc. So set yourself a future goal with your passion and
+                  hobbies!
+                </p>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  className="Home__ButtonSignUp"
+                >
+                  SIGN UP TODAY!
+                </Button>
+              </div>
+            </Grid>
+            <Grid item direction="row" alignItems="center" lg={6} sm={6}>
+              <img
+                src={Event}
+                alt="Image_column"
+                className="Home__ImageFluid"
+              />
+            </Grid>
+          </Grid>
+        </Container>
+      </div>
     </div>
   );
 }
--- client/src/components/Navbar/NavigationBar.js
+++ client/src/components/Navbar/NavigationBar.js
@@ -1,20 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./NavigationBar.scss";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
 import { Button } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 import LogoBlue from "../../Logo/LogoBlue.png";
+import logoWithout from "../../Logo/White.png";
+import WhiteLogo from "../../Logo/Logo-with-white-text.png";
 
 const NavigationBar = () => {
+  const [showNav, setShowNav] = useState(true);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (scrollY > 100) {
+      }
+    });
+  }, []);
+
   return (
-    <AppBar position="static" className="NavigationBar__NavBar">
+    <AppBar position="static" className={showNav ? "NavigationBar__NavBar__active": "NavigationBar__NavBar"}>
+      <div className="NavigationBar___ResponsiveIcon">
+        <MenuIcon className="NavigationBar__Icon" />
+      </div>
       <Toolbar className="NavigationBar__AppBar">
-        <img src={LogoBlue} alt="" />
-        <div className="Buttons">
-          <Button className="NavigationBar__LoginButton">Login</Button>
+        <img
+          src={logoWithout}
+          alt="Logo-without-text"
+          className="NavigationBar__LogoWithout"
+        />
+        <img src={LogoBlue} alt="LogoBlue" className="NavigatioBar__LogoBlue" />
+        <img
+          src={WhiteLogo}
+          alt="WhiteLogo"
+          className="NavigationBar__WhiteLogo"
+        />
+        <div className="NavigationBar__Toolbar">
+          <div className="Buttons">
+            <Button color="primary" className="Home__LinkButtons">
+              Home
+            </Button>
+            <Button color="primary" className="Home__LinkButtons">
+              About
+            </Button>
+            <Button color="primary" className="Home__LinkButtons">
+              Contact
+            </Button>
+
+            <Button className="NavigationBar__LoginButton">Sign up</Button>
+            <Button className="NavigationBar__LoginButton">Login</Button>
+          </div>
         </div>
       </Toolbar>
     </AppBar>
