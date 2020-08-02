0773cbc6434ebdb6918ed22fd1cf54928b0458f5
KosmosKey <khamidovmaga2002@gmail.com>
2020-08-02 14:46:27 +0200

    Installed dependencies for the backend

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`The port is currently running on PORT ${PORT}`)
+);
